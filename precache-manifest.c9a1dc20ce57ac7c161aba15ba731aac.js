141a331c58f1cc2c77d758c80295b0099883d2cb
Felix <51464337+ashotofmilk@users.noreply.github.com>
2020-10-17 20:30:43 -0400

    Updates

---
+++ precache-manifest.c9a1dc20ce57ac7c161aba15ba731aac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53040fd6799ec8a70be352bad52ec6b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "603f0b9489ca351c3c54",
+    "url": "/static/css/2.d54bb455.chunk.css"
+  },
+  {
+    "revision": "603f0b9489ca351c3c54",
+    "url": "/static/js/2.fb6e9bdc.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/static/js/2.fb6e9bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b8914c6531d126d5aa",
+    "url": "/static/js/3.02e2a509.chunk.js"
+  },
+  {
+    "revision": "6130171fdcf489b7dce1",
+    "url": "/static/js/4.c7d07182.chunk.js"
+  },
+  {
+    "revision": "2a5c78525e9d0be9743b",
+    "url": "/static/js/5.d27fbfff.chunk.js"
+  },
+  {
+    "revision": "42a9e8d32c339cf562f6",
+    "url": "/static/js/main.87082a98.chunk.js"
+  },
+  {
+    "revision": "8c92d9eb0c634ba9df64",
+    "url": "/static/js/runtime-main.8c6236e6.js"
+  }
+]);
