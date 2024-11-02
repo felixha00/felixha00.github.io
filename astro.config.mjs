a599368fba1df5b69dcaea7022662ac970f081aa
felix <51464337+felixha00@users.noreply.github.com>
2024-11-01 22:48:10 -0400

    build: install svelte and react deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [tailwind({
+        applyBaseStyles: false,
+    }), react(), svelte()],
+})
