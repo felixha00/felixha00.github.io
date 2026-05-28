ac68262011858afa43963e5be49114e5a54aa3f1
felix <51464337+felixha00@users.noreply.github.com>
2026-05-28 19:44:28 -0400

    fix: scroll to top when navigating to another page

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import { ReactLenis } from "lenis/react";
+import { ReactLenis, useLenis } from "lenis/react";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+    const lenis = useLenis();
+    const pathname = usePathname();
+
+    useEffect(() => {
+        lenis?.scrollTo(0, { immediate: true });
+    }, [pathname, lenis]);
+
+    return null;
+}
 
 export default function SmoothScrolling({ children }: { children: React.ReactNode }) {
     return (
-        <ReactLenis
-            root
-            autoRaf
-            options={{
-                duration: 1,
-            }}
-        >
+        <ReactLenis root autoRaf options={{ duration: 1 }}>
+            <ScrollToTop />
             {children}
         </ReactLenis>
     );
